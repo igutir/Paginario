5bccc2ab4c77f30666786d4d19c387d3992806f2
CamiCodeCR <cam.concha@duocuc.cl>
2024-09-10 23:58:37 -0300

    pruebas estilos home

---
+++ Paginario/paginarioweb/static/js/script.js
@@ -0,0 +1,5 @@
+// Toggle sidebar collapse/expand
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    let sidebarWrapper = document.getElementById("sidebar-wrapper");
+    sidebarWrapper.classList.toggle("collapsed");
+  });
--- Paginario/paginarioweb/urls.py
+++ Paginario/paginarioweb/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home
+from .views import base
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
+    path('base/', base, name='base'),
 ]
