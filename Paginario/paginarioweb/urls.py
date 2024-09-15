da4879226f97d77dd9a10e0eaff2ae94619b6558
Israel Gutierrez <igutierrez@live.cl>
2024-09-15 05:01:02 -0300

    Mantenedor de libros
    Mantenedor de libros + CRUD

--- Paginario/paginarioweb/urls.py
+++ Paginario/paginarioweb/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from .views import home
 from .views import base
+from .views import libro, mantenedor_libros, agregar_libro, modificar_libro_lista, modificar_libro, eliminar_libro
 
 urlpatterns = [
     path('', home, name='home'),
     path('home/', home, name='home'),
     path('base/', base, name='base'),
+
+    # Mantenedor de Administradores:
+    path('j/<int:id>/', libro, name="libro"),
+    path('mantenedor/', mantenedor_libros, name="mantenedor_libros"),
+    path('mantenedor/agregar_libro/', agregar_libro, name="agregar_libro"),
+    path('mantenedor/listado_libros/', modificar_libro_lista, name="modificar_libro_lista"),
+    path('mantenedor/listado_libros/u/<int:idlibro>/', modificar_libro, name="modificar_libro"),
+    path('mantenedor/listado_libros/d/<idlibro>/', eliminar_libro, name="eliminar_libro"),
+
 ]
--- Paginario/paginarioweb/views.py
+++ Paginario/paginarioweb/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+
+from .forms import FomrularioLibro
+from .models import Libro
 
 def home(request):
     return render(request, "index.html")
@@ -6,3 +10,98 @@ def home(request):
 
 def base(request):
     return render(request, "base.html")
+
+
+
+def libro(request, id):
+
+    libro = get_object_or_404(Libro, id = id)
+
+    data = {
+        'libro' : libro
+    }
+
+    return render(request, "libro.html", data)
+
+## Mantenedor de libros:
+"""
+## Permisos en base a lo otorgado al perfil de administrador en el panel de administrador
+@login_required(login_url = "login/")
+@permission_required(['Paginario.add_libro', 'Paginario.delete_libro'], login_url = "login/")
+"""
+def mantenedor_libros(request):
+
+    return render(request, "mantenedor/libro/mantenedor_libros.html")
+
+"""
+## Permisos en base a lo otorgado al perfil de administrador en el panel de administrador
+@login_required(login_url = "login/")
+@permission_required(['Paginario.add_libro'], login_url = "login/")
+"""
+def agregar_libro(request):
+
+    data = {
+        "form_libro": FomrularioLibro,
+        "mensaje": ""
+    }
+
+    if request.method == "POST":
+        formulario = FomrularioLibro(data = request.POST, files = request.FILES)
+
+        if formulario.is_valid:
+            formulario.save()
+            data["mensaje"] = "Libro agregado"
+        else:
+            data["mensaje"] = "Error"
+            data["form"] = formulario
+
+    return render(request, "mantenedor/libro/agregar.html", data)
+"""
+@login_required(login_url = "login/")
+@permission_required(['Paginario.change_libro', 'Paginario.delete_libro'], login_url = "login/")
+"""
+def modificar_libro_lista(request):
+
+    libros = Libro.objects.all()
+
+    data = {
+        'libros' : libros
+    }
+
+    return render(request, "mantenedor/libro/listado_libros.html", data)
+
+"""
+@login_required(login_url = "login/")
+@permission_required(['Paginario.change_libro'], login_url = "login/")
+"""
+def modificar_libro(request, idlibro):
+
+    libro = get_object_or_404(Libro, id = idlibro)
+
+    data = {
+        "form_libro": FomrularioLibro(instance = libro)
+    }
+
+    if request.method == "POST":
+        formulario = FomrularioLibro(data = request.POST, instance = libro, files = request.FILES)
+
+        if formulario.is_valid:
+            formulario.save()
+            return redirect(to="modificar_libro_lista")
+        else:
+            data["mensaje"] = "Error"
+            data["form"] = formulario
+
+    return render(request, "mantenedor/libro/modificar.html", data)
+
+"""
+@login_required(login_url="login/")
+@permission_required(['Paginario.delete_libro'], login_url = "login/")
+"""
+def eliminar_libro(idlibro):
+
+    libro = get_object_or_404(Libro, id = idlibro)
+
+    libro.delete()
+
+    return redirect(to = "modificar_libro_lista")
